9606ca2677d13c1dcd533172c99967b302bc4b14
yhau739 <leeyeehauwork@gmail.com>
2021-11-24 14:16:24 +0800

    first code

--- Pyramid-creator.py
+++ Pyramid-creator.py
@@ -8,14 +8,17 @@
 #  00000000000
 # 0000000000000
 
-def pyramid_creator(pyramid_width):
-    for i in range(0,pyramid_width):
-        for l in range(0,pyramid_width-i):
-            print(" ",end="")
-        for j in range(0,i * 2 + 1):
-            print("0",end="")
-        print("")
+class Pyramid():
+    def pyramid_creator(pyramid_width):
+        for i in range(0,pyramid_width):
+            for l in range(0,pyramid_width-i):
+                print(" ",end="")
+            for j in range(0,i * 2 + 1):
+                print("0",end="")
+            print("")
 
+    width = int(input("Enter the width of the pyramid: "))
+    pyramid_creator(width)
 
-pyramid_creator(7)
+Pyramid()
 
